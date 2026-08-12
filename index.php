c810a81990deee9a1b302deccf6558c0dd5d6190
Laboratorio <laboratorio@escola.local>
2026-08-12 11:00:53 -0300

    form

--- index.php
+++ index.php
@@ -8,18 +8,19 @@
     <div class="col-md-4"></div>
 
     <div class="col-md-4">
-    <div class="card">
-        <div class="card-body">
-                <form action=" " method="post">
-                <label for="login">Username</label>
-                <imput class="form-control" type="text" name="ligin" id="login"/>
-                <label for="senha">Senha</label>
-                <imput class="form-control" type="password" name="senha" id="senha"/>
+        <div class="card">
+            <div class="card-body">
+                <form action="" method="post">
+                    <label for="login">Username</label>
+                    <imput class="form-control" type="text" name="ligin" id="login"></imput>
+                    <label for="senha">Senha</label>
+                    <imput class="form-control" type="password" name="senha" id="senha"></imput>
 
                 </form>
-            
-        
-</div>
+            </div>
+        </div> 
+    </div> 
+
 
     <div class="col-md-4"></div>
     
