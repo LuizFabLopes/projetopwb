9b99d79e78086f3292421317959f1f3f64f2a6bd
Laboratorio <laboratorio@escola.local>
2026-08-19 10:45:48 -0300

    usando php e botão strap

--- cabecalho.php
+++ cabecalho.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/reset.css"/>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -43,5 +44,5 @@
             </div>
         </div>
     </nav>
-    <div class="container">
+    <div class="container h-75 d-flex align-items-center" style="border:1px solid #ff0000">
         
--- index.php
+++ index.php
@@ -1,21 +1,45 @@
 <?php include "cabecalho.php"?>
-<style>
-    div{
-        border:1px solid #ff0000;
-    }
-</style>
-<div class="row">
-    <div class="col-md-4"></div>
+<?php
 
+    if(isset($_POST["login"]))
+    {
+        //Procedimento para banco de dados
+    }
+    if(empty($_POST["login"]))
+    {
+        $mensasgemErro = "Preencha o campo de login";
+    }
+?>
+<div class="row w-100">
+    <div class="col-md-4"></div>
     <div class="col-md-4">
         <div class="card">
             <div class="card-body">
-                <form action="" method="post">
-                    <label for="login">Username</label>
-                    <imput class="form-control" type="text" name="ligin" id="login"></imput>
-                    <label for="senha">Senha</label>
-                    <imput class="form-control" type="password" name="senha" id="senha"></imput>
 
+            <?php
+                if(isset($mensasgemErro))
+                {
+                    
+                    echo '<div class="alert alert-danger">';
+                    echo $mensasgemErro;
+                    echo '</div>';
+                    
+                }
+            ?>
+                <form action="index.php" method="post">
+                    <label for="login">Username</label>
+                    <input class="form-control" type="text" name="ligin" id="login"/>
+                    <label for="senha">Senha</label>
+                    <input class="form-control" type="password" name="senha" id="senha"/>
+                    <div class="row mt-3">
+                        <div class="col-md-6"><button type="submit" class="btn btn-primary">
+                                Entrar
+                            </button></div>
+                            
+                        <div class="col-md-6 align-item-center d-flex justify-content-center">
+                            <input type="checkbox" class="form-check-input mx-2" value="senha"/>Salvar Senha
+                        </div>
+                    </div>
                 </form>
             </div>
         </div> 
@@ -23,6 +47,5 @@
 
 
     <div class="col-md-4"></div>
-    
 </div>
 <?php include "rodape.php"?>
